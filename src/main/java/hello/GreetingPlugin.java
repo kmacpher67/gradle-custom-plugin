ddd91049b0aa3ef59755d0f322861580ab0575a7
kenmac <ken.macpherson@philips.com>
2016-06-09 17:02:03 -0400

    broken error writing archives pom-default.xml

--- src/main/java/hello/GreetingPlugin.java
+++ src/main/java/hello/GreetingPlugin.java
@@ -4,16 +4,50 @@ import hello.tasks.DemoTask;
 import org.gradle.api.*;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.DependencySet;
+import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency;
 
 /**
  * Created by 310237766 on 6/8/2016.
  */
 public class GreetingPlugin implements Plugin<Project> {
 
+    public static final String FIRST_TASK = "build";  // or build or what? compileJava
+    public static final String COMPILE_CONFIGURATION_NAME = "compile";
+
     @Override
     public void apply(Project project) {
+        System.out.println(" GreetingPlugin apply for demo-time!!!!");
         project.getExtensions().create("demoSetting", DemoPluginExtension.class);
+
         project.getTasks().create("demo", DemoTask.class);
+        project.getTasks().getByName("demo").setDescription("");
+        System.out.println(" making the build task depend on demo");
+        // We need to modify the versions before we do the pom get.
+        // can we make our "demo" a pre-req for target module's compile
+        project.getTasks().getByName(FIRST_TASK).dependsOn("demo");
+
+        ConfigurationContainer configurations = project.getConfigurations();
+        Configuration compileConfiguration = configurations.getByName(COMPILE_CONFIGURATION_NAME);
+        showDependencies(compileConfiguration);
+        //"org.projectlombok:lombok:1.16.8"
+        Dependency dependency = new DefaultExternalModuleDependency("org.projectlombok", "lombok", "1.16.8", "compile");
+        compileConfiguration.getDependencies().add(dependency);
+        System.out.println(" AFTER add ing goodies");
+        showDependencies(compileConfiguration);
+        System.out.println(" It is DONE!! The magic has started. ");
+}
+    private void showDependencies(Configuration compileConfiguration){
+        DependencySet compiledependencySet = compileConfiguration.getAllDependencies();
+        System.out.println("COMPILE compiledependencySet");
+        for(Dependency dependency : compiledependencySet){
+            System.out.print("dependency get Group()=" + dependency.getGroup() );
+            System.out.print(" : Name=" + dependency.getName() );
+            System.out.println(" : Version()=" + dependency.getVersion() );
+        }
     }
 
 }
--- src/test/groovy/hello/DemoPluginTest.groovy
+++ src/test/groovy/hello/DemoPluginTest.groovy
@@ -1,6 +1,12 @@
 package hello
 
 import hello.tasks.DemoTask
+import org.gradle.api.DefaultTask
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.api.internal.artifacts.dependencies.DefaultDependencyArtifact
+import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
 import org.junit.Test
 import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.api.Project
@@ -9,9 +15,31 @@ import static org.junit.Assert.*
 class DemoPluginTest {
     @Test
     public void demo_plugin_should_add_task_to_project() {
+        println(" TESTING first task" + GreetingPlugin.FIRST_TASK)
         Project project = ProjectBuilder.builder().build()
+
+        // add a dummy task to project for dependency check
+        project.getTasks().create(GreetingPlugin.FIRST_TASK, DefaultTask.class);
+        project.configurations.create(GreetingPlugin.COMPILE_CONFIGURATION_NAME)
         project.getPlugins().apply 'greetingPlugin'
-        println("project.tasks is...:" + project.defaultTasks.toString())
+        println("project.tasks build is...:" + project.tasks.build.actions.size())
         assertTrue(project.tasks.demo instanceof DemoTask)
+    }
+
+    private Project setupProject(Project project){
+
+        String dependencyNotation = "compile 'o.demo:DoItDemo:6.6.6'";
+        //Configuration dependencyNotation = project.getConfigurations().maybeCreate(GreetingPlugin.COMPILE_CONFIGURATION_NAME);
+//        dependency.name = "DoItDemo";
+//        dependency.version = "6.6.6";
+//        def dependencyNotation = [:];//'group': 'com.google.code.guice', 'name': 'guice', 'version': '1.0'
+//        dependencyNotation.put(group,'com.google.code.guice')
+//        dependencyNotation.put(name,'guice')
+//        dependencyNotation.put(version: '1.0')
+        //Dependency dependency = new DefaultExternalModuleDependency('com.google.code.guice', 'guice','6.6.6', 'compile');
+        //dependencyNotation.dependencies.add(dependency)
+        Dependency dependency = project.dependencies.add(GreetingPlugin.COMPILE_CONFIGURATION_NAME, dependencyNotation);
+        println(" dependency.name=" + dependency.name)
+        return project;
     }
 }
